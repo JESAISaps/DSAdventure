abd8b57959c772278202755c7182b6252943c8a9
JESAISap <nicolas.strekowski@gmail.com>
2025-04-06 20:02:44 +0200

    cleared main

--- main.py
+++ main.py
@@ -6,26 +6,6 @@ import keyboard
 
 player = Player("Jean", 0)
 
-menu = Menu("Home")
-shop = Shop("Shop")
-
-actualRoom = menu
-
-print(actualRoom.RoomIntroduction())
-print("Que veux tu faire")
-
-#hehe = click.prompt("Quelle salle", type=int, show_choices=True)
-
-
-question = questionary.select(
-    "What do you want to do?",
-    choices=[
-        'Order a pizza',
-        'Make a reservation',
-        'Ask for opening hours'
-    ])
-
-question.ask()
 
 isQPressed = False
 presse = keyboard.read_hotkey()
